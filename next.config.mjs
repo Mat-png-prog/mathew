75f7ec2cb6d9f51755d2087808f32b44c566c07f
Mat04 <stopglobalwarming622@gmail.com>
2025-04-10 11:52:18 +0000

    basic setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  cacheMaxMemorySize: 3000,
+  experimental:{
+    serverActions: {
+      enabled: true,
+    },
+    serverComponentsExternalPackages:["node/rs/argon2"]
+  },
+  productionBrowserSourceMaps: false,
+  reactProductionProfiling: false,
+  reactStrictMode: true,
+};
 
 export default nextConfig;
